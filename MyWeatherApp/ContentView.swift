3e9646e3e11ce1bb173ad4d378017788370c33ad
Vineet <44131643+vineetg3@users.noreply.github.com>
2024-01-21 12:47:36 -0600

    Initial Commit

---
+++ MyWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyWeatherApp
+//
+//  Created by Vineet Gandham on 1/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by Vineet Gandham on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
